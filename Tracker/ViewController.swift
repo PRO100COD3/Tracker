dcd631a86957745b4ed9c8f94049f52d1da27157
PRO100COD3 <vad.dz2015@yandex.ru>
2024-06-25 21:59:06 +0400

    start

--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class TrackersViewController: UIViewController {
+class ViewController: UIViewController {
 
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "Hello, World!"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Press me", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        // Добавляем метку и кнопку в представление
+        view.addSubview(label)
+        view.addSubview(button)
+        
+        // Настройка constraints для метки
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        // Настройка constraints для кнопки
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20)
+        ])
+        
+        // Добавляем действие для кнопки
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
     }
-
-
+    
+    @objc func buttonPressed() {
+        label.text = "Button Pressed!"
+    }
 }
-
